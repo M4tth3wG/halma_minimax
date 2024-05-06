c1234f2fc2af738610a0eeda2220c30575cf3a4a
Mateusz Gazda <matigazda29@gmail.com>
2024-05-07 00:00:18 +0200

    Refactor heuristics

--- heuristics.py
+++ heuristics.py
@@ -4,17 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def winning_coordinates(player):
-    coordinates = []
-
-    if player == FieldType.PLAYER_WHITE:
-        coordinates = halma.Halma.camp_coordinates[FieldType.PLAYER_BLACK]
-    else:
-        coordinates = halma.Halma.camp_coordinates[FieldType.PLAYER_WHITE]
-
-    return coordinates
-
-
 CAMP_EDGES = {
     FieldType.PLAYER_WHITE : [(halma.START_AREA_SIZE + 1 - y, y) for y in range(1, halma.START_AREA_SIZE + 1)],
     FieldType.PLAYER_BLACK : [(halma.BOARD_SIZE - halma.START_AREA_SIZE + y - 2, halma.BOARD_SIZE - y - 1) for y in range(1, halma.START_AREA_SIZE + 1)]
@@ -56,8 +45,7 @@ def print_heatmap(grid):
     plt.show()
 
 HEATMAPS = {player : create_heatmap(player) for player in halma.Halma.players}
-PLAYER_WHITE_WINNING_COORDINATES = winning_coordinates(FieldType.PLAYER_WHITE)
-PLAYER_BLACK_WINNING_COORDINATES = winning_coordinates(FieldType.PLAYER_BLACK)
+WINNING_COORDINATES = {player : halma.Halma.camp_coordinates[halma.get_next_player(player)] for player in halma.Halma.players}
 
 def manhattan_distance(point_a, point_b):
     point_a_x, point_a_y = point_a
@@ -68,16 +56,19 @@ def manhattan_distance(point_a, point_b):
 def euclides_distance(point_a, point_b):
     return math.dist(point_a, point_b)
 
+def combined_distance(point_a, point_b, *distance_functions):
+    return sum(func(point_a, point_b) for func in distance_functions)
+
 def manhattan_end_positon_heuristic(move, halma_game):
     player = halma_game.current_player
     (start_x, start_y), (end_x, end_y) = move
 
     if player == FieldType.PLAYER_WHITE:
         corner_x, corner_y = halma.BOARD_SIZE - 1, halma.BOARD_SIZE - 1
-        winning_coordinates = PLAYER_WHITE_WINNING_COORDINATES
+        winning_coordinates = WINNING_COORDINATES[FieldType.PLAYER_WHITE]
     else:
         corner_x, corner_y = 0, 0
-        winning_coordinates = PLAYER_BLACK_WINNING_COORDINATES
+        winning_coordinates = WINNING_COORDINATES[FieldType.PLAYER_BLACK]
 
     if (start_x, start_y) in winning_coordinates:
         start_factor = (abs(start_x - corner_x) + abs(start_y - corner_y))
@@ -96,114 +87,37 @@ def calculate_distance_to_camp(field_coordinates, camp_coordinates, distance_fun
     distances = [distance_function(field_coordinates, camp_field) for camp_field in camp_coordinates]
     return min(distances) # changed
 
-# def manhattan_state_heuristic(board, maximizing_player):
-#     player_white_fields = halma.calculate_player_fields(board, FieldType.PLAYER_WHITE)
-#     player_black_fields = halma.calculate_player_fields(board, FieldType.PLAYER_BLACK)
+def distance_state_heuristic(board, maximizing_player, distance_function):
+    player_fields = {player : halma.calculate_player_fields(board, player) for player in halma.Halma.players}
+    free_end_zone_fields = {player : list(set(WINNING_COORDINATES[player]) - set(player_fields[player])) for player in halma.Halma.players} # change
+    heuristic_value = {player : 0 for player in halma.Halma.players}
+    player_goal_corner = {
+        FieldType.PLAYER_WHITE : (15, 15),
+        FieldType.PLAYER_BLACK : (0,0)
+    }
 
-#     player_white_sum = sum([calculate_distance_to_camp(field, halma.Halma.camp_coordinates[FieldType.PLAYER_WHITE], manhattan_distance) for field in player_white_fields])
-#     player_black_sum = sum([calculate_distance_to_camp(field, halma.Halma.camp_coordinates[FieldType.PLAYER_BLACK], manhattan_distance) for field in player_black_fields])
+    for player in halma.Halma.players:
+        for field in player_fields[player]:
+            heuristic_value[player] = heuristic_value[player] + 4 * (halma.BOARD_SIZE - 1) # 2 * distance_function(player_goal_corner[player], player_goal_corner[halma.get_next_player(player)])
+            
+            if field not in WINNING_COORDINATES[player]:
+                heuristic_value[player] = heuristic_value[player] - calculate_distance_to_camp(field, free_end_zone_fields[player], distance_function)
+            
+            heuristic_value[player] = heuristic_value[player] - distance_function(field, player_goal_corner[player])
+
+    minimizing_player = halma.get_next_player(maximizing_player)
+    heuristic_value[minimizing_player] = heuristic_value[minimizing_player] * -1
     
-#     if maximizing_player == FieldType.PLAYER_WHITE:
-#         player_black_sum = -1 * player_black_sum
-#     else:
-#         player_white_sum = -1 * player_white_sum
-
-#     return player_white_sum + player_black_sum
-
-# def manhattan_state_heuristic(board, maximizing_player):
-#     player_white_fields = halma.calculate_player_fields(board, FieldType.PLAYER_WHITE)
-#     player_black_fields = halma.calculate_player_fields(board, FieldType.PLAYER_BLACK)
-
-#     player_white_free_end_zone_fields = list(set(PLAYER_WHITE_WINNING_COORDINATES) - set(player_white_fields))
-#     player_black_free_end_zone_fields = list(set(PLAYER_BLACK_WINNING_COORDINATES) - set(player_black_fields))
-
-#     player_white_sum = 0
-#     player_black_sum = 0
-
-#     for field in player_white_fields:
-#         player_white_sum = player_white_sum + 2 * (halma.BOARD_SIZE - 1)
-        
-#         if field not in PLAYER_WHITE_WINNING_COORDINATES:
-#             player_white_sum = player_white_sum - calculate_distance_to_camp(field, player_white_free_end_zone_fields, manhattan_distance)
-
-#     for field in player_black_fields:
-#         player_black_sum = player_black_sum + 2 * (halma.BOARD_SIZE - 1)
-        
-#         if field not in PLAYER_BLACK_WINNING_COORDINATES:
-#             player_black_sum = player_black_sum - calculate_distance_to_camp(field, player_black_free_end_zone_fields, manhattan_distance)
-
-#     if maximizing_player == FieldType.PLAYER_WHITE:
-#         player_black_sum = -1 * player_black_sum
-#     else:
-#         player_white_sum = -1 * player_white_sum
-
-#     return player_white_sum + player_black_sum
+    return sum(heuristic_value[player] for player in halma.Halma.players)
 
 def manhattan_state_heuristic(board, maximizing_player):
-    player_white_fields = halma.calculate_player_fields(board, FieldType.PLAYER_WHITE)
-    player_black_fields = halma.calculate_player_fields(board, FieldType.PLAYER_BLACK)
-
-    player_white_free_end_zone_fields = list(set(PLAYER_WHITE_WINNING_COORDINATES) - set(player_white_fields))
-    player_black_free_end_zone_fields = list(set(PLAYER_BLACK_WINNING_COORDINATES) - set(player_black_fields))
-
-    player_white_sum = 0
-    player_black_sum = 0
-
-    for field in player_white_fields:
-        player_white_sum = player_white_sum + 4 * (halma.BOARD_SIZE - 1)
-        
-        if field not in PLAYER_WHITE_WINNING_COORDINATES:
-            player_white_sum = player_white_sum - calculate_distance_to_camp(field, player_white_free_end_zone_fields, manhattan_distance)
-        
-        player_white_sum = player_white_sum - manhattan_distance(field, (15,15))
-
-    for field in player_black_fields:
-        player_black_sum = player_black_sum + 4 * (halma.BOARD_SIZE - 1)
-        
-        if field not in PLAYER_BLACK_WINNING_COORDINATES:
-            player_black_sum = player_black_sum - calculate_distance_to_camp(field, player_black_free_end_zone_fields, manhattan_distance)
-
-        player_black_sum = player_black_sum - manhattan_distance(field, (0,0))
-
-    if maximizing_player == FieldType.PLAYER_WHITE:
-        player_black_sum = -1 * player_black_sum
-    else:
-        player_white_sum = -1 * player_white_sum
-
-    return player_white_sum + player_black_sum
+    return distance_state_heuristic(board, maximizing_player, manhattan_distance)
 
 def euclides_state_heuristic(board, maximizing_player):
-    player_white_fields = halma.calculate_player_fields(board, FieldType.PLAYER_WHITE)
-    player_black_fields = halma.calculate_player_fields(board, FieldType.PLAYER_BLACK)
+    return distance_state_heuristic(board, maximizing_player, euclides_distance)
 
-    player_white_free_end_zone_fields = list(set(PLAYER_WHITE_WINNING_COORDINATES) - set(player_white_fields))
-    player_black_free_end_zone_fields = list(set(PLAYER_BLACK_WINNING_COORDINATES) - set(player_black_fields))
-
-    player_white_sum = 0
-    player_black_sum = 0
-
-    for field in player_white_fields:
-        player_white_sum = player_white_sum + 4 * (halma.BOARD_SIZE - 1)
-        
-        if field not in PLAYER_WHITE_WINNING_COORDINATES:
-            player_white_sum = player_white_sum - calculate_distance_to_camp(field, player_white_free_end_zone_fields, euclides_distance)
-        
-        player_white_sum = player_white_sum - euclides_distance(field, (15,15))
-
-    for field in player_black_fields:
-        player_black_sum = player_black_sum + 4 * (halma.BOARD_SIZE - 1)
-        
-        if field not in PLAYER_BLACK_WINNING_COORDINATES:
-            player_black_sum = player_black_sum - calculate_distance_to_camp(field, player_black_free_end_zone_fields, euclides_distance)
-
-        player_black_sum = player_black_sum - euclides_distance(field, (0,0))
-
-    if maximizing_player == FieldType.PLAYER_WHITE:
-        player_black_sum = -1 * player_black_sum
-    else:
-        player_white_sum = -1 * player_white_sum
-
-    return player_white_sum + player_black_sum
+def manhattan_euclides_state_heuristic(board, maximizing_player):
+    return distance_state_heuristic(board, maximizing_player, lambda a, b : combined_distance(a, b, manhattan_distance, euclides_distance))
 
 def heatmap_heuristic(board, maximizing_player):
     heatmap = HEATMAPS[maximizing_player]
--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def main():
     # player_white_strategy = strategies.BestNextStateStrategy(heuristics.manhattan_state_heuristic)
     # player_white_strategy = strategies.MinimaxStrategy(heuristics.manhattan_state_heuristic, 1)
     # player_white_strategy = strategies.AlphaBetaStrategy(heuristics.manhattan_state_heuristic, 2)
-    player_white_strategy = strategies.BestNextStateStrategy(heuristics.manhattan_state_heuristic)
-    player_black_strategy = strategies.BestNextStateStrategy(heuristics.euclides_state_heuristic)
+    player_white_strategy = strategies.BestNextStateStrategy(heuristics.manhattan_euclides_state_heuristic)
+    player_black_strategy = strategies.BestNextStateStrategy(heuristics.manhattan_state_heuristic)
 
     game = Game(player_white_strategy, player_black_strategy)
     run(game, verbous=False)
